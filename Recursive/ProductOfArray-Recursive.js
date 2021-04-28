8a71892cf141825d4a2650c786a01ec721b7d050
Kailash <kailash.prabhu23@gmail.com>
2021-04-28 19:20:58 +0530

    Cleanup

--- Recursive/ProductOfArray-Recursive.js
+++
@@ -1,6 +0,0 @@
-function productOfArray(arr) {
-    if(arr.length < 1) return 1;
-    return arr[0] * productOfArray(arr.splice(1));
-}
-
-console.log(productOfArray([2, 2, 3]))
--- Recursive/ReverseAString-Recursively.js
+++
@@ -1,6 +0,0 @@
-function reverse(string){
-  if(string.length < 1) return "";
-  return string[string.length - 1] + reverse(string.slice(0, string.length - 1))
-}
-
-console.log(reverse('hello'))
