70c244cc94ee3b01ca7b4928a611ad1dc0f849c5
Kailash <kailash.prabhu23@gmail.com>
2021-05-11 00:55:45 +0530

    Altered

--- searchingAlgorithm/Naive_String_Search.js
+++ searchingAlgorithm/Naive_String_Search.js
@@ -2,14 +2,11 @@ function stringSearch(input_string, search_string) {
     let matches_found = 0;
     for (let i = 0; i < input_string.length; i++) {
         for (let j = 0; j < search_string.length; j++){
-            if(search_string[j] !== input_string[i]) break;
-            else if (search_string[j] == input_string[i]) {
-                i++;
-                if(j == (search_string.length - 1)) matches_found++;
-            }
+            if(search_string[j] !== input_string[i + j]) break;
+            if(j == (search_string.length - 1)) matches_found++;
         }
     }
-    return matches_found
+    return matches_found;
 }
 
-console.log(stringSearch("wowomgzomg", "omga"))
+console.log(stringSearch("lolol loled", "lol"))
